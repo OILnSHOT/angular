cc7eb472741344cea8d5fcfbbfe526839a3f6aae
OILnSHOT <OILnSHOT@users.noreply.github.com>
2022-12-07 18:10:20 +0100

    created angular app

---
+++ skokinar/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import '../../src/styles.css';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'skokinar';
+}
